88f96799265b1cf790f511cc375ff83708aef8ad
lucas-jorge <lucasajorge@gmail.com>
2024-03-08 09:48:23 -0300

    database WIP

--- app/models/models.py
+++ app/models/models.py
@@ -22,3 +22,19 @@ class Avaliacao(db.Model):
     aluno_leitura = db.Column(db.Text, nullable=True)
     crdi = db.Column(db.Float, nullable=True)
     id_avaliacao = db.Column(db.Integer, nullable=True)
+
+class Aluno(db.Model):
+    __tablename__ = 'alunos'
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(120), nullable=False)
+    cpf = db.Column(db.String(14), nullable=True)
+    data_nascimento = db.Column(db.Date, nullable=True)
+    escola = db.Column(db.String(120), nullable=True)
+    turma = db.Column(db.String(120), nullable=True)
+    serie = db.Column(db.String(120), nullable=True)
+    curso = db.Column(db.String(120), nullable=True)
+    ano = db.Column(db.Integer, nullable=False)
+    turno = db.Column(db.String(120), nullable=True)
+    nome_mae = db.Column(db.String(120), nullable=True)
+    nome_pai = db.Column(db.String(120), nullable=True)
+    nome_responsavel = db.Column(db.String(120), nullable=True)
